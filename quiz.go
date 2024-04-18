7be80a171f26c0b0ef4efe5faca1ee385f93fe81
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-18 17:34:33 +0530

    Created LICENCE

---
+++ quiz.go
@@ -0,0 +1,8 @@
+// We need to have a package name on top of every go file
+// This is the main package
+// This is the entry point of the application
+// We can have only one main package in a project
+// We can have multiple packages in a project
+// We can have multiple files in a package
+// We can have multiple functions in a file
+package main
