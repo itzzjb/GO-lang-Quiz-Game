62a72630eabb595e0e18cbcd35689348d29aad5f
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-18 22:10:53 +0530

    hello world

--- quiz.go
+++ quiz.go
@@ -1,8 +1,23 @@
-// We need to have a package name on top of every go file
+// We need to have a package name on top of every go file (it is mandatory)
 // This is the main package
 // This is the entry point of the application
 // We can have only one main package in a project
 // We can have multiple packages in a project
-// We can have multiple files in a package
-// We can have multiple functions in a file
+// We can have multiple files in a package (files are also called as modules)
 package main
+
+import "fmt"
+
+// Importing the required packages
+// fmt - format package
+// This allows us to print the output to the console and to read the input from the console
+// This gives access to some tools build into the standard Go library.
+
+// This is the main function
+// It is the entry point of the application
+// It is the first function to be executed when the application is running (This is a executable function)
+func main() {
+	// P is capital in Println
+	// No need to use semicolon at the end of the statement
+	fmt.Println("Hello World")
+}
