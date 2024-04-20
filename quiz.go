91b0f781cc671ecfd4c82d5876884e0625d92714
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-20 11:44:40 +0530

    welcome screen done

--- quiz.go
+++ quiz.go
@@ -16,8 +16,43 @@ import "fmt"
 // It is the entry point of the application
 // It is the first function to be executed when the application is running (This is a executable function)
 func main() {
+
 	// P is capital in Println
 	// Println -> Prints the output to the console and moves the cursor to the next line
 	// No need to use semicolon at the end of the statement
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to my quiz game!")
+
+	// Datatype cannot be changed for a variable once it is declared
+	// var name string = "Tim"
+	// You can also declare a variable without specifying the data type too. Go will automatically detect the data type
+	// var name = "Tim"
+	// name := "Tim"
+	// Note -> In go, if you declare a variable, you have to use it. Otherwise, it will throw an error
+
+	// Declaring a variable for name without assigning a value
+	var name string
+
+	// To get input from the user, we can use Scanln
+	// Scan -> reads the input from the console and stores it in the variable and moves the cursor to the next line
+	// Scanln -> reads the input from the console and stores it in the variable and does not move the cursor to the next line
+	// & -> address of the variable (pointer) -> memory address of the variable in RAM
+	// If the user enters a value in a different data type, it will give the default value of the data type of the variable
+	fmt.Printf("What is your name: ") // We use Printf here to avoid going to the next line
+	fmt.Scan(&name)
+
+	// We can use printf to format the output (Printf -> Print formatted)
+	// %v -> value
+	// %d -> decimal
+	// %f -> float
+	// %s -> string
+	// \n -> new line
+	fmt.Printf("Hello %v, Welcome to the game!\n", name) // fmt.Println("Hello", name)
+
+	// Some data type in GO
+	// string -> "Hello"
+	// int -> 10 (signed integer)
+	// uint -> 10 (unsigned integer)
+	// float64, float32 -> 10.5
+	// bool -> true , false
+
 }
